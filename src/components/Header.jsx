124e4a33d70354f0297bda5e0b3efc12d6fabed8
StahMarina <stahmarinaoleg@gmail.com>
2020-03-31 04:34:12 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://image.flaticon.com/icons/png/512/202/premium/202306.png"/>
+        </header>
+    );
+}
+
+export default Header;
